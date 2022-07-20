2ccde98021a16722a0900eee1cf0e731c715dbca
Jiabin Huang <j.huang@uke.de>
2022-07-20 15:04:44 +0200

    Update plotTree.R

--- plotTreeHeatmap/plotTree.R
+++ plotTreeHeatmap/plotTree.R
@@ -3,9 +3,9 @@ library(ggplot2)
 
 setwd("~/DATA/plotTreeHeatmap/")
 
-info <- read.csv("typing_188.csv")
+info <- read.csv("typing_186.csv")
 info$name <- info$Isolate
-tree <- read.tree("raxml_567.tree")
+tree <- read.tree("raxml_567_186.tree")
 cols <- c(infection='purple2', commensal='skyblue2')     
 
 library(dplyr)
@@ -28,10 +28,10 @@ p <- ggtree(tree, layout='circular', branch.length='none') %<+% info +
 #difference between geom_tiplab and geom_tiplab2?
 #+ theme(axis.text.x = element_text(angle = 30, vjust = 0.5)) + theme(axis.text = element_text(size = 20))  + scale_size(range = c(1, 20))
 #font.size=10, 
-png("ggtree.png", width=1200, height=1200)
+png("ggtree.png", width=1260, height=1260)
 p
 dev.off()
 
-png("ggtree_and_gheatmap.png", width=1200, height=1200)
+png("ggtree_and_gheatmap.png", width=1290, height=1000)
 gheatmap(p, heatmapData2, width=0.1,colnames_position="top", colnames_angle=90, colnames_offset_y = 0.1, hjust=0.5, font.size=4, offset = 8) + scale_fill_manual(values=heatmap.colours) +  theme(legend.text = element_text(size = 14)) + theme(legend.title = element_text(size = 14)) + guides(fill=guide_legend(title=""), color = guide_legend(override.aes = list(size = 5)))  
 dev.off()
